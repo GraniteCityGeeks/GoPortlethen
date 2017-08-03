0be517ce4570dec81d23c325bfda86912415f4e9
Medscootsman <murraylyne2@Gmail.com>
2017-08-03 20:48:26 +0100

    route on map

--- HTML/assets/js/map.js
+++ HTML/assets/js/map.js
@@ -4,4 +4,35 @@ function initMap() {
     center: {lat:57.05474, lng:-2.13066},
     zoom: 15
   });
+
+//directions
+
+var directionsService = new google.maps.DirectionsService;
+var directionDisplay = new google.maps.DirectionsRenderer( {
+  draggable:true,
+  map:map,
+
+});
+
+//directionDisplay.addListener("directions_changed" function() {
+//  computeTotalDistance(directionDisplay.getDirections());
+//});
+
+renderRoute("Aberdeen", "Edinburgh", directionsService, directionDisplay);
+
+function renderRoute(origin, destination, service, display) {
+  service.route({
+    origin: origin,
+    destination: destination,
+    travelMode: 'DRIVING',
+    avoidTolls: true
+  }, function(response, status) {
+    if (status === "OK") {
+      display.setDirections(response);
+    } else {
+      alert("Murray's an idiot, here's an error code so you can correct him: " +
+       status);
+    }
+  });
+}
 }
