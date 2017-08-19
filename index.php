4bd8fefa5a91f258aae847eeb900f8aae16c586d
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-08-19 14:15:14 +0100

    css

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
     <!-- Favicon -->
     <link href="favicon.ico" rel="shortcut icon">
     <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="HTML/assets/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="/HTML/assets/css/bootstrap.css" rel="stylesheet">
     <!-- Template CSS -->
-    <link rel="stylesheet" href="HTML/assets/css/animate.css" rel="stylesheet">
-    <link rel="stylesheet" href="HTML/assets/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="HTML/assets/css/nexus.css" rel="stylesheet">
-    <link rel="stylesheet" href="HTML/assets/css/responsive.css" rel="stylesheet">
-    <link rel="stylesheet" href="HTML/assets/css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="/HTML/assets/css/animate.css" rel="stylesheet">
+    <link rel="stylesheet" href="/HTML/assets/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="/HTML/assets/css/nexus.css" rel="stylesheet">
+    <link rel="stylesheet" href="/HTML/assets/css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="/HTML/assets/css/custom.css" rel="stylesheet">
     <!-- Google Fonts-->
     <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
 </head>
