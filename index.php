692f26d469ee7f327425fd3c015ff2ab47a23751
Jamie Law <jamiepeterlaw@gmail.com>
2017-08-24 22:18:47 +0100

    server testing

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ $uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
 $uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
 $uri = urldecode($uri);
 echo $uri;
+echo dirname($_SERVER['SCRIPT_NAME']);
 foreach ($rules as $action => $rule) {
     if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
         include(INCLUDE_DIR . $action . '.php');
