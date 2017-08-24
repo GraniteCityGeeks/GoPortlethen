23c733654cf2152c30d7087c4bc52c62e6ebf8fe
JordanYoungman <jwyoungman96@hotmail.co.uk>
2017-08-24 20:37:03 +0100

    test

--- inc/Home.php
+++ inc/Home.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['username'])) {
 <main>
 <p>Welcome {$_SESSION['username']}! You have an access level of: {$_SESSION['accessLevelID']} your userID is: {$_SESSION['userID']} and Profile pic is: <img src={$_SESSION['photoID']} alt=\"Mountain View\" style=\"width:304px;height:228px;\"></p>
 </main>
-"
+";
 } else {
     echo "
 <main>
